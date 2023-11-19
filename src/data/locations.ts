872dbb93d341674c0a5aae29f77d943d72ff632f
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-20 00:42:59 +0300

    Changed locations primary key to allow null, as this table is populated manully and can not be altered by client side nor users

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -1,12 +1,13 @@
-import { Table, Column, Model, DataType } from 'sequelize-typescript';
+import { Table, Column, Model, DataType, AutoIncrement } from 'sequelize-typescript';
 
 @Table({tableName: "locations", timestamps: false, freezeTableName: true })
 export default class Locations extends Model {
 
+  @AutoIncrement
   @Column({
     type: DataType.INTEGER,
     primaryKey: true,
-    allowNull: false,
+    allowNull: true,
   })
   location_id?: number;
 
