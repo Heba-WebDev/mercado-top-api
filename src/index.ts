f1bc857accc368e22a5bb9691d8a6cb91b51a0e9
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-20 16:34:52 +0300

    Added resource and global error handling

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import sequelize from "./data/db";
 import { userRouter } from "./routes/users.route";
+import { CustomError } from "./interfaces/customError";
+import { statusCode } from "./utils/httpStatusCode";
+const { FAIL } = statusCode;
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +13,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRouter);
+
+app.use((error: CustomError, req: Request, res: Response, next: NextFunction): void => {
+  res.status(error?.statusCode || 500).send({status: FAIL, mesaage: error.message})
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).send({
+    status: FAIL,
+    message: "Resource not found."
+  })
+})
 
 const connectToDB =  async () => {
   try {
