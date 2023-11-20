45e70f7ea739e4372a3336b13a082da6a67fc217
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-20 16:35:54 +0300

    Added global handling and a wrapper function for controllers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,23 +2,25 @@ import { Response, Request, NextFunction } from "express";
 import Users from "../data/users";
 import { hash } from "bcrypt";
 import Locations from "../data/locations";
+import { wrapper } from "../middlewares/asyncWrapper";
+import { statusCode } from "../utils/httpStatusCode";
+import { globalError } from "../utils/globalError";
+const { SUCCESS, FAIL } = statusCode;
 
-const signup = async(req: Request, res: Response, next: NextFunction) => {
+const signup = wrapper(async(req: Request, res: Response, next: NextFunction) => {
 const { name, email, password, country } = req.body;
 if (!name || !email || !password || !country) {
-    return res.status(400).send({
-        message: "Name, Email, Password and country are required.",
-        data: null
-    })
+    const err = new globalError("Name, Email, Password and country are required.", 400
+    ,FAIL)
+    return next(err);
 }
 
 const location = await Locations.findOne({where: {country: country}});
 
 if (!location) {
-    return res.status(400).send({
-        message: "Please provide a valid country name.",
-        data: null
-    })
+    const err = new globalError("Please provide a valid country name.", 400
+    ,FAIL)
+    return next(err);
 }
 
 const hashedPassword = await hash(password, 10);
@@ -32,6 +34,7 @@ Users.create({
 })
 .then((result) => {
     return res.status(201).send({
+        status: SUCCESS,
         message: "User successfully created.",
         data: {
             name: name,
@@ -40,13 +43,7 @@ Users.create({
         }
     })
 })
-.catch((error) => {
-    return res.status(500).send({
-        message: "Oops! Something went wrong.",
-        error: error.message,
-    })
 })
-}
 
 export {
     signup
