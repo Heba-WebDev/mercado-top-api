73b252a0a7a3db34cba33bd3f01404d023b0f568
Heba-Webdev23 <jtvonline@yahoo.com>
2023-11-21 01:25:55 +0300

    Added signin and forgot password routes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { signup } from "../controllers/users.controller";
+import { signup, signin, forgotPassword } from "../controllers/users.controller";
 
 const userRouter = Router();
 
 userRouter.route("/signup").post(signup);
+userRouter.route("/signin").post(signin);
+userRouter.route("/forgotPassword").post(forgotPassword);
 
 export {
     userRouter
