0868a7d7ccbcc6b01b30181cb4f76d426c318dd4
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-19 19:57:15 +0300

    Changes db dialect back to mySQL

--- src/data/db.ts
+++ src/data/db.ts
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(
   process.env.PASSWORD as string,
   {
     host: process.env.HOST,
-    dialect: "postgres",
+    dialect: "mysql",
     define: {
         freezeTableName: true,
     },
