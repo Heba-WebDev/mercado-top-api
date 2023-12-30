f0fc79f120a23f6d50960e970cab6763a6757b57
Heba-Webdev23 <jtvonline@yahoo.com>
2023-12-30 17:39:54 +0300

    Added a updateUser route to handle updating the user details, like name, email, password, profile picture and country

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { signup, signin, forgotPassword, resetPassword, getUserById } from "../controllers/users.controller";
+import { signup, signin, forgotPassword, resetPassword, getUserById, updateUser } from "../controllers/users.controller";
+import upload from "../middlewares/multer";
 
 const userRouter = Router();
 
@@ -8,6 +9,8 @@ userRouter.route("/signin").post(signin);
 userRouter.route("/forgotPassword").post(forgotPassword);
 userRouter.route("/resetPassword").post(resetPassword);
 userRouter.route("/").get(getUserById);
+userRouter.route("/updateUser").put(upload.fields([{ name: 'profile_picture', maxCount: 1 },
+{ name: 'name' }, { name: 'email' }, { name: 'password' }, { name: 'country' }]), updateUser);
 
 export {
     userRouter
